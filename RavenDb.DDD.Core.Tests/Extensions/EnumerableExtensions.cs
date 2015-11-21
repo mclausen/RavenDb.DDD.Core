14402d0f078144569b217be2b645f6ac5c109990
Martin Humlund Clausen <martinh.c@me.com>
2015-11-21 21:57:52 +0100

    Added unittest

---
+++ RavenDb.DDD.Core.Tests/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RavenDb.DDD.Core.Extensions;
+
+namespace RavenDb.DDD.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class EnumerableExtensionsTest
+    {
+        [Test]
+        public void ShuffleDoesShuffleCollection()
+        {
+            var list = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            Assert.That(list, Is.Ordered);
+
+            list.Shuffle();
+            Assert.That(list, Is.Not.Ordered);
+        }
+    }
+}
